11eb4dfb943b93e8b54a09a24bd62451ef2b1890
MaxFeldman1 <mackx@protonmail.com>
2020-06-09 03:09:37 -0500

    improved tests and added helper file that lets evm time travel

--- contracts/oracle.sol
+++ contracts/oracle.sol
@@ -6,7 +6,8 @@ contract oracle{
     uint public latestSpot;
 
     //lists all block heights at which spot is collected
-    uint[] heights;
+    uint[] public heights;
+    function heightsLength() external view returns (uint length) {length = heights.length;}
     //height => timestamp
     mapping(uint => uint) public timestamps;
     //timestamp => price
@@ -38,6 +39,7 @@ contract oracle{
         heights.push(block.number);
         heights.push(block.number);
         heights.push(block.number);
+        //set();
     }
 
     function set() public {
@@ -49,17 +51,16 @@ contract oracle{
         mostRecent = startHeight;
     }
     
-    
     function tsToIndex(uint _time) public view returns (uint) {
-        if (_time >= timestamps[heights[heights.length-1]]) return latestSpot;
-        if (_time < timestamps[heights[0]] || heights.length < 3) return 0;
-        if (tsToSpot[_time] != 0) return tsToSpot[_time];
         uint size = heights.length;
+        if (_time >= timestamps[heights[size-1]]) return size-1;
+        if (_time < timestamps[heights[0]] || size < 3) return 0;
+        //if (tsToSpot[_time] != 0) return tsToSpot[_time];
         uint step = size>>2;
         for (uint i = size>>1; ;){
             uint currentTs = timestamps[heights[i]];
             uint prevTs = i < 1 ? 0 : timestamps[heights[i-1]];
-            uint nextTs = i+1 < heights.length ? timestamps[heights[i+1]]: timestamps[heights[heights.length-1]];
+            uint nextTs = i+1 < size ? timestamps[heights[i+1]]: timestamps[heights[size-1]];
             /*
                 p => prevTs
                 c => currentTs
@@ -71,11 +72,46 @@ contract oracle{
                 p, Target, c, n => p
                 Target, p, c, n => decreace i
             */
-            if (_time > nextTs)
-                i = (i+step) < heights.length ? i+step : heights.length-1;                
-            else if (_time > currentTs)
+            if (_time >= nextTs)
+                i = (i+step) < size ? i+step : size-1;                
+            else if (_time >= currentTs)
                 return i;
-            else if (_time > prevTs)
+            else if (_time >= prevTs)
+                return i-1;
+            else
+                i = i > step ? i-step : 0;
+            step = (step>>1) > 0? step>>1: 1;
+        }
+
+    }
+
+    function heightToIndex(uint _height) public view returns (uint) {
+        uint size = heights.length;
+        if (_height >= heights[size-1]) return size-1;
+        if (_height <= heights[0] || size == 3) return 0;
+        uint step = size>>2;
+        for (uint i = size>>1; ;){
+            uint currentHeight = heights[i];
+            uint prevHeight = i < 1 ? 0 : heights[i-1];
+            uint nextHeight = i+1 < size ? heights[i+1]: heights[size-1];
+            /*
+                p => prevTs
+                c => currentTs
+                n => nextTs
+                Target => _time
+                    On each iteration find where Target is in relation to others
+                p, c, n, Target => increace i
+                p, c, Target, n => c
+                p, Target, c, n => p
+                Target, p, c, n => decreace i
+            */
+            if (_height > nextHeight)
+                i = (i+step) < size ? i+step : size-1;
+            else if (_height == nextHeight)
+                return i+1 < size ? i+1 : size-1;
+            else if (_height >= currentHeight)
+                return i;
+            else if (_height >= prevHeight)
                 return i-1;
             else
                 i = i > step ? i-step : 0;
@@ -86,6 +122,7 @@ contract oracle{
 
     function medianPreviousIndecies(uint _index) public view returns (uint) {
         require(_index > 1, "index must be 2 or greater");
+        require(_index < heights.length, "index must be in array");
         uint first = tsToSpot[timestamps[heights[_index-2]]];
         uint second = tsToSpot[timestamps[heights[_index-1]]];
         uint third = tsToSpot[timestamps[heights[_index]]];
@@ -95,12 +132,8 @@ contract oracle{
         return second;
     }
 
-    function tsToPrice(uint _time) external view returns (uint) {
+    function fetchSpotAtTime(uint _time) external view returns (uint) {
         return medianPreviousIndecies(tsToIndex(_time));
-    }
-
-    function height() public view returns(uint){
-        return block.number;
     }
 
 }
--- test/oracle.js
+++ test/oracle.js
@@ -4,6 +4,8 @@ const uniswapPair = artifacts.require('./UniswapV2Pair.sol');
 const uniswapERC20 = artifacts.require('./UniswapV2ERC20.sol');
 const oracle = artifacts.require("./oracle.sol");
 
+const helper = require("../helper/helper.js");
+
 contract('oracle', async function(accounts) {
 
 	web3.eth.defaultAccount = accounts[0];
@@ -23,6 +25,8 @@ contract('oracle', async function(accounts) {
 
 		subUnits = Math.pow(10, (await asset0.decimals()));
 
+		inflator = (await oracleInstance.inflator()).toNumber();
+
 		} catch (err) {
 			console.error(err.message);
 			throw(err);
@@ -36,11 +40,111 @@ contract('oracle', async function(accounts) {
 			pairInstance.transfer(pairInstance.address, liquidityBalance);
 			await pairInstance.burn(web3.eth.defaultAccount);
 		}
-		asset0.transfer(pairInstance.address, Math.floor(subUnits*spot));
-		asset1.transfer(pairInstance.address, subUnits);
+		var balance0 = await asset0.balanceOf(pairInstance.address);
+		var balance1 = await asset1.balanceOf(pairInstance.address);
+		asset0.transfer(pairInstance.address, Math.floor(subUnits*spot) - balance0);
+		await asset1.transfer(pairInstance.address, subUnits - balance1);
 		await pairInstance.mint(web3.eth.defaultAccount);
 		await oracleInstance.set();
 	}
+
+	async function heightToPrevTs(height) {
+		var index = (await oracleInstance.heightToIndex(height)).toNumber();
+		var newHeight = (await oracleInstance.heights(index)).toNumber();
+		return (await oracleInstance.timestamps(newHeight)).toNumber();
+	}
+
+	async function heightToPrevSpot(height) {
+		var index = (await oracleInstance.heightToIndex(height)).toNumber();
+		var newHeight = (await oracleInstance.heights(index)).toNumber();
+		var newTs = (await oracleInstance.timestamps(newHeight)).toNumber();
+		return (await oracleInstance.tsToSpot(newTs)).toNumber();
+	}
+
+	async function tsToPrevSpot(time) {
+		var index = (await oracleInstance.tsToIndex(time)).toNumber();
+		var newHeight = (await oracleInstance.heights(index)).toNumber();
+		var newTs = (await oracleInstance.timestamps(newHeight)).toNumber();
+		return (await oracleInstance.tsToSpot(newTs)).toNumber();
+	}
+
+	async function indexToSpot(index) {
+		var height = (await oracleInstance.heights(index)).toNumber();
+		var ts = (await oracleInstance.timestamps(height)).toNumber();
+		return (await oracleInstance.tsToSpot(ts)) / inflator;
+	}
+
+	//in solidity block.number is always height of the next block, in web3 it is height of prev block
+	function getBlockNumber() {
+		return web3.eth.getBlockNumber();
+	}
+
+	it('sets and fetches spot price', async () => {
+		try{
+			spot = 5
+			secondSpot = 7;
+			await setPrice(spot);
+			blockSetSpot = await getBlockNumber();
+			await helper.advanceTime(2);
+			res = (await oracleInstance.latestSpot()) / inflator;
+			assert.equal(res, spot, 'latestSpot() fetches current spot price');
+			height = await getBlockNumber();
+			res = (await heightToPrevSpot(height))/inflator;
+			//res = (await heightToPrevSpot(height))/inflator;
+			assert.equal(res, spot, "getUint(uint) fetches the latest spot price");
+			await setPrice(secondSpot);
+			blockSetSecondSpot = await getBlockNumber();
+			await helper.advanceTime(2);
+			//note that we have not updated the value of height yet
+			res = (await heightToPrevSpot(height)) / inflator;
+			assert.equal(res, spot, "getUint(uint) can fetch previous values");
+			//we are now feching the price of the blocks after setting the spot a second time
+			res = (await heightToPrevSpot(blockSetSecondSpot+5))/inflator;
+			assert.equal(res, secondSpot, "getUint(uint) can fetch the most recent spot");
+			res = (await heightToPrevSpot(height-3))/inflator;
+			assert.equal(res, 0, "getUint(uint) returns 0 when there are no previous spot prices");
+			res = await web3.eth.getBlock('latest');
+			height = res.number;
+			time = res.timestamp;
+			result = (await heightToPrevSpot(height))/inflator;
+			//res  = await oracleInstance.timestampBehindHeight(height);
+			res  = await heightToPrevTs(height);
+			assert.equal(res <= time, true, "returns the correct timestamp");
+			await setPrice(1);
+			blockSet1 = await getBlockNumber();
+			await helper.advanceTime(2);
+			await setPrice(5);
+			blockSet5 = await getBlockNumber();
+			await helper.advanceTime(2);
+			await setPrice(6);
+			blockSet6 = await getBlockNumber();
+			res = await web3.eth.getBlock('latest');
+			diff = res.timestamp-time;
+			time = res.timestamp;
+			height = res.number;
+			res = (await tsToPrevSpot(time))/inflator;
+			assert.equal(res, 6, "correct spot");
+			newTime = (await web3.eth.getBlock(blockSet1)).timestamp+1;
+			res = (await tsToPrevSpot(newTime))/inflator;
+			assert.equal(res, 1, "correct spot");
+			newTime = (await web3.eth.getBlock(blockSet5)).timestamp+1;
+			res = (await tsToPrevSpot(newTime))/inflator;
+			assert.equal(res, 5, "correct spot");
+			newTime = (await web3.eth.getBlock(blockSetSpot)).timestamp;
+			spotTime = newTime;
+			res = (await tsToPrevSpot(newTime))/inflator;
+			assert.equal(res, spot, "correct spot");
+			newTime = (await web3.eth.getBlock(blockSetSecondSpot)).timestamp;
+			res = (await tsToPrevSpot(newTime))/inflator;
+			assert.equal(res, secondSpot, "correct spot");
+			res = (await tsToPrevSpot(spotTime-4))/inflator;
+			assert.equal(res, 0, "correct spot");
+		} catch (err) {
+			console.error(err.message);
+			throw err;
+		}
+
+	});
 
 	it('sets / reads price correctly from uniswap', async() => {
 		try {
@@ -56,5 +160,92 @@ contract('oracle', async function(accounts) {
 	});
 
 
-	//it('gets median pri')
+	it('gets median', async () => {
+		//set time in the future so that it will always be ahead of current time
+		time = parseInt((new Date()).getTime()/1000)*2;
+		await setPrice(1);
+		await helper.advanceTime(2);
+		await setPrice(2);
+		await helper.advanceTime(2);
+		await setPrice(10);
+		var median = 2;
+		var length = (await oracleInstance.heightsLength()).toNumber();
+		assert.equal(await indexToSpot(length-1), 10, 'correct spot at last index');
+		assert.equal(await indexToSpot(length-2), 2, 'correct spot at 2nd to last index');
+		assert.equal(await indexToSpot(length-3), 1, 'correct spot at 3rd to last index');
+
+
+		//last 3 elemets orderd by spot from 1=>smallest 2=>median 3=>largest [1, 2, 3]
+		res = (await oracleInstance.fetchSpotAtTime(time))/inflator;
+		assert.equal(res, median, "correct median of last 3 spots [1, 2, 3]");
+		await helper.advanceTime(2);
+		await setPrice(6);
+		median = 6;
+		//last 3 elemets orderd by spot from 1=>smallest 2=>median 3=>largest [1, 3, 2]
+		res = (await oracleInstance.fetchSpotAtTime(time))/inflator;
+		assert.equal(res, median, "correct median of last 3 spots [1, 3, 2]");
+		await helper.advanceTime(2);
+		await setPrice(3);
+		median = 6;
+		//last 3 elemets orderd by spot from 1=>smallest 2=>median 3=>largest [3, 2, 1]
+		res = (await oracleInstance.fetchSpotAtTime(time))/inflator;
+		assert.equal(res, median, "correct median of last 3 spots [3, 2, 1]");
+		await helper.advanceTime(2);
+		await setPrice(4);
+		median = 4;
+		//last 3 elemets orderd by spot from 1=>smallest 2=>median 3=>largest [3, 1, 2]
+		res = (await oracleInstance.fetchSpotAtTime(time))/inflator;
+		assert.equal(res, median, "correct median of last 3 spots [3, 1, 2]");
+		await helper.advanceTime(2);
+		await setPrice(2);
+		median = 3;
+		//last 3 elemets orderd by spot from 1=>smallest 2=>median 3=>largest [2, 3, 1]
+		res = (await oracleInstance.fetchSpotAtTime(time))/inflator;
+		assert.equal(res, median, "correct median of last 3 spots [2, 3, 1]");
+		await helper.advanceTime(2);
+		await setPrice(6);
+		median = 4;
+		//last 3 elemets orderd by spot from 1=>smallest 2=>median 3=>largest [2, 1, 3]
+		res = (await oracleInstance.fetchSpotAtTime(time))/inflator;
+		assert.equal(res, median, "correct median of last 3 spots [2, 1, 3]");
+
+		//now test for when two of the last three elements have the same value
+		await helper.advanceTime(2);
+		await setPrice(2);
+		median = 2;
+		//last 3 elemets orderd by spot from 1=>smallest 2=>largest [1, 2, 1]
+		res = (await oracleInstance.fetchSpotAtTime(time))/inflator;
+		assert.equal(res, median, "correct median of last 3 spots [1, 2, 1]");
+		await helper.advanceTime(2);
+		await setPrice(2);
+		median = 2;
+		//last 3 elemets orderd by spot from 1=>smallest 2=>largest [2, 1, 1]
+		res = (await oracleInstance.fetchSpotAtTime(time))/inflator;
+		assert.equal(res, median, "correct median of last 3 spots [2, 1, 1]");
+		await helper.advanceTime(2);
+		await setPrice(3);
+		median = 2;
+		//last 3 elemets orderd by spot from 1=>smallest 2=>largest [1, 1, 2]
+		res = (await oracleInstance.fetchSpotAtTime(time))/inflator;
+		assert.equal(res, median, "correct median of last 3 spots [1, 1, 2]");
+		await helper.advanceTime(2);
+		await setPrice(3);
+		median = 3;
+		//last 3 elemets orderd by spot from 1=>smallest 2=>largest [1, 2, 2]
+		res = (await oracleInstance.fetchSpotAtTime(time))/inflator;
+		assert.equal(res, median, "correct median of last 3 spots [1, 2, 2]");
+		await helper.advanceTime(2);
+		await setPrice(2);
+		median = 3;
+		//last 3 elemets orderd by spot from 1=>smallest 2=>largest [2, 2, 1]
+		res = (await oracleInstance.fetchSpotAtTime(time))/inflator;
+		assert.equal(res, median, "correct median of last 3 spots [2, 2, 1]");
+		await helper.advanceTime(2);
+		await setPrice(3);
+		median = 3;
+		//last 3 elemets orderd by spot from 1=>smallest 2=>largest [2, 1, 2]
+		res = (await oracleInstance.fetchSpotAtTime(time))/inflator;
+		assert.equal(res, median, "correct median of last 3 spots [2, 1, 2]");
+	});
+
 });
